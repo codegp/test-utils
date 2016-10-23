14d685e2aeea38a884d74f4c8682d22d74cc2b6e
davidmarne <davemarne@gmail.com>
2016-10-22 23:00:16 -0600

    gofmt

--- setuprunner/setuprunner.go
+++ setuprunner/setuprunner.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-  "flag"
-  "log"
+	"flag"
+	"log"
 
-  "github.com/codegp/test-utils"
-  "github.com/codegp/cloud-persister"
+	"github.com/codegp/cloud-persister"
 	"github.com/codegp/kube-client"
+	"github.com/codegp/test-utils"
 )
 
 var (
-  testUtils *testutils.TestUtils
-	forceBuild   *bool
-	runGame *bool
+	testUtils  *testutils.TestUtils
+	forceBuild *bool
+	runGame    *bool
 )
 
 func init() {
@@ -20,23 +20,23 @@ func init() {
 	fatalOnErr(err)
 	kclient, err := kubeclient.NewClient()
 	fatalOnErr(err)
-  testUtils = testutils.NewTestUtils(cp, kclient)
+	testUtils = testutils.NewTestUtils(cp, kclient)
 
-  forceBuild = flag.Bool("fb", false, "force a new build even if one already exists, bool")
+	forceBuild = flag.Bool("fb", false, "force a new build even if one already exists, bool")
 	runGame = flag.Bool("r", false, "run test game, bool")
 	flag.Parse()
 }
 
 func main() {
-  if *forceBuild {
-    fatalOnErr(testUtils.BuildTestGametype(true, true))
-  }
+	if *forceBuild {
+		fatalOnErr(testUtils.BuildTestGametype(true, true))
+	}
 
-  if *runGame {
-    fatalOnErr(testUtils.RunTestGame(true))
-  } else if !*forceBuild {
-    fatalOnErr(testUtils.BuildTestGametype(true, false))
-  }
+	if *runGame {
+		fatalOnErr(testUtils.RunTestGame(true))
+	} else if !*forceBuild {
+		fatalOnErr(testUtils.BuildTestGametype(true, false))
+	}
 }
 
 func fatalOnErr(err error) {
